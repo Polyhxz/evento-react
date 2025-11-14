33a764ad012dafd1a3c1c80ecfe35b3fcbac5a79
Polyhxz <polyana_lemes@estudante.sesisenai.org.br>
2025-11-14 16:06:30 -0300

    Atu

--- src/paginas/CadastroEvento.jsx
+++ src/paginas/CadastroEvento.jsx
@@ -1,4 +1,4 @@
-import React, { UseState, useState } from "react";
+import React, { UseState } from "react";
 
 function CadastroEvento ({ onCadastro }) {
     //Estados dos campos do formulário 
@@ -16,6 +16,49 @@ function Enviar(a) {
         nome, 
         data, 
         local, 
-        };
-    }
+    };
+    
+
+    //Limpa os campos após cadastrar 
+    setNome("");
+    setData("");
+    setLocal("");
 }
+
+return (
+    <div className="cadastroEvento-container">
+        <h2> Cadastrar Evento</h2>
+
+        <form onSubmit={enviar} className="form-evento"> 
+
+        <input
+        tyoe="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        required
+        />
+
+        <label>Data:</label>
+        <input
+        type="date"
+        value={data}
+        obnChange={(e) => setData(e.target.value)}
+        required
+        />
+
+        <label>Local:</label>
+        <input
+        type="text"
+        value={value}
+        onChange={(e) => setLocal}
+        />
+
+        <button type="submit" className="btn-salvar"> Salvar Evento </button>
+        <button type="submit" className="btn-salvar"> Voltar </button>
+        </form>
+    </div>
+
+    );
+}
+
+export default CadastroEvento;
